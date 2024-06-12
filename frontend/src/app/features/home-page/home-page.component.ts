667b4bb36993269e08fdb75a02a23d72669843f9
Anandakrishnan H <sree403020@gmail.com>
2024-06-12 18:38:28 +0530

    Frontend: Installed bootstrap. Added slider in hompage and added 3 banners of chrome seconds

--- frontend/src/app/features/home-page/home-page.component.ts
+++ frontend/src/app/features/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ProductsSectionComponent } from './products-section/products-section.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HeroSectionComponent, ProductsSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
