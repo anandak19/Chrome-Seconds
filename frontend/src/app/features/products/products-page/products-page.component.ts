f8911618dfec4587aa76a7872f1f230bf9ca3ff3
Anandakrishnan H <sree403020@gmail.com>
2024-06-17 16:05:24 +0530

    Frontend: Product page created and displayed all the products from database

--- frontend/src/app/features/products/products-page/products-page.component.ts
+++ frontend/src/app/features/products/products-page/products-page.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { databaseWatchDetails } from '../../../core/models/watch-details';
+import { ProductManagementService } from '../../../shared/services/productServices/product-management.service';
 
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-page.component.html',
-  styleUrl: './products-page.component.scss'
+  styleUrl: './products-page.component.scss',
 })
-export class ProductsPageComponent {
+export class ProductsPageComponent implements OnInit {
+  slectedGender = 'Gender';
+  slectedBrand = 'Brand';
+  slectedCategory = 'Category';
+  selectedDropdown!: string;
+  allProducts: databaseWatchDetails[] = []
 
+  constructor(public _productManagement: ProductManagementService) {}
+
+  // when select a gender
+  selectGender(gender: string) {
+    console.log(gender);
+    this.slectedGender = gender;
+    // hide dropdown
+    this.toggleGenderDropdown(this.selectedDropdown);
+  }
+
+  // when select a brand
+  selectBrand(brand: string) {
+    console.log(brand);
+    this.slectedBrand = brand;
+    // hide dropdown
+    this.toggleGenderDropdown(this.selectedDropdown);
+  }
+
+  // when select a brand
+  selectCategory(category: string) {
+    console.log(category);
+    this.slectedCategory = category;
+    // hide dropdown
+    this.toggleGenderDropdown(this.selectedDropdown);
+  }
+
+  toggleGenderDropdown(dropdown: string) {
+    if (this.selectedDropdown && this.selectedDropdown !== dropdown) {
+      const currentOpenDropdown = document.querySelector(this.selectedDropdown);
+      currentOpenDropdown?.classList.add('select-hide');
+    }
+    this.selectedDropdown = dropdown;
+    const selectItems = document.querySelector(this.selectedDropdown);
+    selectItems?.classList.toggle('select-hide');
+  }
+
+  // to close dropdown
+  hideDropDown() {
+    const selectItems = document.querySelectorAll('.select-items');
+    selectItems.forEach((item) => item.classList.add('select-hide'));
+  }
+
+  // when click anywhere on screen
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const clickedElement = event.target as HTMLElement;
+    if (!clickedElement.closest('.products-container__select')) {
+      this.hideDropDown();
+    }
+  }
+
+  ngOnInit(): void {
+    this._productManagement.getSomeProducts().subscribe(
+      (res) => {
+        this.allProducts = res
+        console.log(this.allProducts);
+        
+        
+      },(error) => {
+        console.error(error);
+        
+      }
+    )
+  }
+  
 }
