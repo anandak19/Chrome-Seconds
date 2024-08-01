54af7682010cf572b5775d5e47ea9d0c7cf5f0c1
Anandakrishnan H <sree403020@gmail.com>
2024-08-01 11:53:15 +0530

    Fixed some bugs

--- frontend/src/app/features/home-page/products-section/products-section.component.ts
+++ frontend/src/app/features/home-page/products-section/products-section.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ProductManagementService } from '../../../shared/services/productServices/product-management.service';
 import { databaseWatchDetails } from '../../../core/models/watch-details';
 import { CommonModule, CurrencyPipe, DecimalPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products-section',
   standalone: true,
-  imports: [DecimalPipe, CommonModule],
+  imports: [DecimalPipe, CommonModule, RouterLink],
   templateUrl: './products-section.component.html',
   styleUrl: './products-section.component.scss'
 })
