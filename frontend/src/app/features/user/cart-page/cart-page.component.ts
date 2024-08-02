5f1236b48e3af118dd3823e7548377edb403acc8
Anandakrishnan H <sree403020@gmail.com>
2024-08-02 13:50:27 +0530

    Fixed some bugs

--- frontend/src/app/features/user/cart-page/cart-page.component.ts
+++ frontend/src/app/features/user/cart-page/cart-page.component.ts
@@ -4,6 +4,8 @@ import { CartProduct } from '../../../core/models/watch-details';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { WindowRefService } from '../../../shared/services/windowRefService/window-ref.service';
 import { OrderService } from '../../../shared/services/orderService/order.service';
+import { UserManagementService } from '../../../shared/services/userServices/user-management.service';
+import { dbUserData } from '../../../core/models/user-details';
 
 @Component({
   selector: 'app-cart-page',
@@ -16,11 +18,26 @@ export class CartPageComponent implements OnInit, OnChanges {
   cartProducts!: CartProduct[];
   deliveryDate!: Date;
   totalAmount: number = 0;
+  userData!: dbUserData;
 
-  constructor(private _cartService: CartService, private _windowRef: WindowRefService, private _orderService: OrderService) {}
+  constructor(
+    private _cartService: CartService,
+    private _windowRef: WindowRefService,
+    private _orderService: OrderService,
+    private _userService: UserManagementService
+  ) {}
 
-
-
+  // get current user data 
+  getUserDetails(){
+    this._userService.getUserDetails().subscribe(
+      (res) => {
+        this.userData = res
+      },(err) =>{
+        console.error(err);
+        alert("User not login")
+      }
+    )
+  }
   // here increaseQuantity and decreaseQuantity share same code, make it one later
 
   increaseQuantity(productId: string): void {
@@ -28,7 +45,7 @@ export class CartPageComponent implements OnInit, OnChanges {
     this._cartService.addCart(productId).subscribe(
       (res) => {
         // console.log(res);
-        this.getCartItems()
+        this.getCartItems();
       },
       (err) => {
         console.error(err);
@@ -67,79 +84,82 @@ export class CartPageComponent implements OnInit, OnChanges {
       this.totalAmount += product.productId.price * product.quantity;
     }
   }
-  
 
   placeOrder(amount: number) {
-    console.log("To pay",amount);
-    
+    console.log('To pay', amount);
+
     //order place logic
     this._orderService.createRzPayOrder(amount).subscribe(
       (res) => {
         console.log('res', res);
         const orderId = res.orderId;
-        const amount = res.amount
+        const amount = res.amount;
 
         // call pay with razor with id,amnt
-        this.payWithRazor(orderId, amount)
-        
-      },(error) => {
+        this.payWithRazor(orderId, amount);
+      },
+      (error) => {
         console.error('Error creating order', error);
-        
       }
-    )
+    );
   }
 
-  payWithRazor(orderId : string, amount: number){
+  payWithRazor(orderId: string, amount: number) {
     const options: any = {
-      key : 'rzp_test_J9Uy26wK6oSemQ',
+      key: 'rzp_test_J9Uy26wK6oSemQ',
       amount: amount,
       currency: 'INR',
       name: 'Chrome Seconds',
       description: '',
-      image: '' ,
+      image: '',
       order_id: orderId,
       modal: {
-        escape: false
+        escape: false,
       },
       theme: {
-        color: '#e04400'
-      }
-    }
+        color: '#e04400',
+        backdrop_color: '#000000a2' 
+      },
+      prefill: {
+        name: this.userData.fullName,
+        email: this.userData.email,
+        contact: this.userData.phone 
+      },
+    };
 
     options.handler = (response: any, error: any) => {
       options.response = response;
       console.log(response);
-      console.log(options);
+      alert('Payment successful!');
       //call api to varify signature and capture transactions
-    }
+      // save the order details to a collections for admin ref 
+    };
 
     options.modal.ondismiss = () => {
-      // when user close the window while transaction in process 
-      console.log("Transaction cancelled");
-    }
+      // when user close the window while transaction in process
+      console.log('Transaction cancelled');
+    };
 
-    const rzp = new this._windowRef.nativeWindow.Razorpay(options)
+    const rzp = new this._windowRef.nativeWindow.Razorpay(options);
     rzp.open();
   }
 
-
-
-
-  
-  // lifecycles 
+  // lifecycles
   ngOnInit(): void {
     window.scrollTo(0, 0);
     const currentDate = new Date();
     this.deliveryDate = new Date(currentDate);
     this.deliveryDate.setDate(this.deliveryDate.getDate() + 5);
     this.getCartItems();
-    // this.calcTotalAmount();
+    // get current user data 
+    this.getUserDetails()
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Changes detected:');
   }
-  
+
   // get cart products
   getCartItems() {
     this._cartService.getCart().subscribe(
@@ -147,7 +167,6 @@ export class CartPageComponent implements OnInit, OnChanges {
         this.cartProducts = res;
         // console.log("Get");
         this.calcTotalAmount();
-        
       },
       (err) => {
         console.error(err);
